6dd511fe190c7b0873177f1fe14dc232fdbc798f
Nikita Kuduhovi <dumb_animal@icloud.com>
2022-02-07 19:21:37 +0300

    Add base routes

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## Что должно содержаться в файле .env
 
 - port (PORT)
+
+## Что используется?
+
+- [JSend][jsend] - JSON спецификация.
+
+[jsend]:https://github.com/omniti-labs/jsend
--- app.js
+++ app.js
@@ -1,21 +1,54 @@
 // LOADING .ENV FILE
 require('dotenv').config();
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
+// MIDDLEWARE
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({ message: 'hello json!', app: 'natours' });
-// });
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/tours.json`));
 
-// app.post('/', (req, res) => {
-//   res.status(200).send('You can post to this endpoint...');
-// });
+app.get('/api/v1/tours', (req, res) => {
+   res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: { tours },
+   });
+});
 
-app.get('/api/v1/touts', (req, res) => {});
+app.post('/api/v1/tours', (req, res) => {
+   const newId = tours[tours.length - 1].id + 1;
+   const newTour = Object.assign({ id: newId }, req.body);
+
+   tours.push(newTour);
+
+   res.status(200).json({
+      status: 'success',
+      data: { tour: newTour },
+   });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+   const id = parseInt(req.params.id);
+   const tour = tours.find((el) => el.id === id);
+
+   if (tour) {
+      res.status(200).json({
+         status: 'success',
+         data: { tour },
+      });
+   } else {
+      res.status(404).json({
+         status: 'fail',
+         massage: 'Not Found',
+      });
+   }
+});
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}...`);
+   console.log(`App running on port ${PORT}...`);
 });
